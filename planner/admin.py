25e77aa6b1ec4f08e4e791d24cc13f110d28e1e4
noodlesen <k.lapshov@gmail.com>
2020-11-09 15:04:03 +0300

    ...

--- planner/admin.py
+++ planner/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from planner.models import Post
+from planner.models import Post, Idea, Resource
 
 # Register your models here.
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'scheduled_date', 'published_date')
+
+admin.site.register(Post, PostAdmin)
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'post_type')
+
+admin.site.register(Idea, IdeaAdmin)
+
+admin.site.register(Resource)
--- planner/models.py
+++ planner/models.py
@@ -10,3 +10,20 @@ class Post(models.Model):
 
     def __str__(self):
         return ('Post: '+self.title)
+
+class Idea(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(default='', blank=True)
+    POST_TYPES = (
+        ('recipe', 'RECIPE',),
+        ('tip', 'TIP',),
+        ('info', 'INFO'),
+        ('other', 'OTHER')
+    )
+    post_type = models.CharField(max_length=255, choices=POST_TYPES, default='recipe')
+
+class Resource(models.Model):
+    link = models.URLField()
+    idea = models.ForeignKey(Idea, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
