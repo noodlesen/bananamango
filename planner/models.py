f9f3ea9eb55b308cb281af9172ee84c2d34773d1
noodlesen <k.lapshov@gmail.com>
2020-11-10 10:27:50 +0300

    added strs

--- planner/models.py
+++ planner/models.py
@@ -12,6 +12,8 @@ class Post(models.Model):
         return ('Post: '+self.title)
 
 class Idea(models.Model):
+    def __str__(self):
+        return self.title
     title = models.CharField(max_length=255)
     description = models.TextField(default='', blank=True)
     POST_TYPES = (
@@ -23,7 +25,9 @@ class Idea(models.Model):
     post_type = models.CharField(max_length=255, choices=POST_TYPES, default='recipe')
 
 class Resource(models.Model):
+    def __str__(self):
+        return self.title
     link = models.URLField()
     idea = models.ForeignKey(Idea, on_delete=models.SET_NULL, null=True, blank=True)
-    title = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255, default='Noname')
     description = models.TextField(null=True, blank=True)
